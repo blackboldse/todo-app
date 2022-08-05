997ce4b0e1ef6efee7bef1385e71968c999e57c7
blackbold <toantran014@gmail.com>
2022-08-05 23:59:05 +0700

    Add image, and initial file setup

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Reading You Dont Know JS",
+    "Learning JavaScript",
+  ]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <h1>Hello react TODO app</h1>
+      <h1>Hello react TODO app 👋</h1>
+
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button type="submit" onClick={addTodo}>
+          Add Todo
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
