f353775d7dfc65bb7425d0e098df13be78bcd09d
blackbold <toantran014@gmail.com>
2022-08-19 22:44:02 +0700

    Initial file with react

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>How to create TODO app with Firebase 🔥</h1>
+    <input />
+    <button>Add todo</button>
+
+    <ul>
+      <li></li>
+    </ul>
     </div>
   );
 }
