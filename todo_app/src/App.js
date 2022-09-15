5bf7b9f921d85a463c9afa77086f577164c1fbdc
blackbold <toantran014@gmail.com>
2022-09-15 08:40:57 +0700

    Add files via upload

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodo] = useState(['Take a walk with dog'])
   return (
     <div className="App">
     <h1>How to create TODO app with Firebase 🔥</h1>
-    <input />
-    <button>Add todo</button>
+    <input value={setTodo} />
+    <button onclick = {todos}>Add todo</button>
 
     <ul>
       <li></li>
     </ul>
     </div>
   );
-}
+}q
 
 export default App;
