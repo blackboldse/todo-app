027dc2ef0823349ca400df5f1cf4ad9819c71f0b
blackbold <toantran014@gmail.com>
2022-09-27 16:28:45 +0700

    Update npm, add infor readme

--- README.md
+++ README.md
@@ -11,10 +11,11 @@
 ### Notes
 
 Prerequisites 2 fundamentals of React:
+
 - STATE: `useState()`, `event.preventDefault()`
-- PROPS: 
+- PROPS:
 
 ### Run dynamic JavsScript with JSX
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Connect with me Instagram:
+@blackboldse
--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
 import './App.css';
 
-function App() {
-  const [todos, setTodo] = useState(['Take a walk with dog'])
+
+export default function App() {
+  const [todos, setTodo] = useState(["Learning ReactJS"]);
+  const [input, setInput] = useState("");
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    setTodo([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-    <h1>How to create TODO app with Firebase 🔥</h1>
-    <input value={setTodo} />
-    <button onclick = {todos}>Add todo</button>
+      <h2>Create a TODO app with Firebase 🔥</h2>
+      <h3 className="subtitle">Add todo get started Today</h3>
+      <form action="">
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        {/* <button onClick={handleAddTodo}>Add todo</button> */}
+        <Button
+          type='submit'
+          variant='contained'
+          color='primary'
+          onClick={handleAddTodo}>Add todo</Button>
+      </form>
 
-    <ul>
-      <li></li>
-    </ul>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
-}q
-
-export default App;
+}
